099653e3fec11705dec6e7d60a53d75a0b27b76f
Erray Dineri <guermazimaram@yahoo.fr>
2024-10-30 10:39:15 +0100

    atelier8

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EtudiantController;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/etudiant', [EtudiantController::class, "index"])->name('etudiant');
-Route::get('/create', [EtudiantController::class,"create"])->name('etudiant.create');
-Route::post('/create', [EtudiantController::class, "store"])->name('etudiant.ajouter');
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/etudiant', [EtudiantController::class,"index"])->name('etudiant');
+    
+    Route::get('/create', [EtudiantController::class,"create"])->name('etudiant.create');
+    Route::post('/create', [EtudiantController::class,"store"])->name('etudiant.ajouter');
+    
+    Route::get('/etudiant/{etudiant}', [EtudiantController::class,"edit"])->name('etudiant.edit');
+    Route::put('/etudiant/{etudiant}', [EtudiantController::class,"update"])->name('etudiant.update');
+    
+    Route::get('/show/{etudiant}', [EtudiantController::class,"show"])->name('etudiant.show');
 
-Route::put('/etudiant/{etudiant}', [EtudiantController::class, "update"])->name('etudiant.update');
-Route::get('/etudiant/{etudiant}', [EtudiantController::class, "edit"])->name('etudiant.edit');
-
-Route::get('/show/{etudiant}', [EtudiantController::class, "show"])->name('etudiant.show');
-
-Route::delete('/delete/{etudiant}', [EtudiantController::class,"delete"])->name('etudiant.delete');
+    Route::delete('/delete/{etudiant}', [EtudiantController::class,"delete"])->name('etudiant.delete');
+    });
